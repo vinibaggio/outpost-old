df7db9b7d6206c9ff82ae3f7c885d38664416e49
Vinicius Baggio <vinibaggio@gmail.com>
2010-08-23 22:52:31 -0300

    Adding response code hook and it's test

--- lib/outpost/scout/hooks/response_code.rb
+++ lib/outpost/scout/hooks/response_code.rb
@@ -2,15 +2,28 @@ module Scout
   module Hooks
     module ResponseCode
 
-      def after_measurement(response)
-        @response_code = response
-        super(response)
+      def before_measurement
+        puts "ResponseCode#before_measurement"
       end
 
-
-      def build_report(response, rules={})
-        super(response, rules)
+      def after_measurement
+        puts "ResponseCode#after_measurement"
       end
+
+      def build_report(all_rules={})
+        rules = all_rules.delete(:response_code)
+        status_list = []
+
+        if rules
+          rules.each do |rule_pair|
+            status_list += rule_pair.map do |rule, status|
+              status if rule == @response
+            end
+          end
+          consolidate(status_list)
+        end
+      end
+
     end
   end
 end
