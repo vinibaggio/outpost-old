51e5e10819ae0df6cf514fbfa71401f52e7ce7ed
Vinicius Baggio <vinibaggio@gmail.com>
2010-08-23 22:55:55 -0300

    Adding basic DSL support

--- lib/outpost.rb
+++ lib/outpost.rb
@@ -1,1 +1,38 @@
 require 'outpost/scout'
+
+class Outpost
+  @@scouts = []
+  @@reports = {}
+
+  class << self
+    def depends(dependencies, &block)
+      dependencies.each do |scout, name|
+        @@current_scout = scout
+      end
+      class_eval(&block)
+      @@scouts << @@current_scout.new(@@options)
+    end
+
+    def report(status, rules)
+      @@reports[@@current_scout] ||= {}
+      @@reports[@@current_scout][rules.keys.first] = rules.values.map { |val| {val => status} }
+    end
+
+    def options(options={})
+      @@options = options
+    end
+
+    def check!
+      @@scouts.each { |scout| scout.measure! }
+      report_status
+    end
+
+    private
+
+    def report_status
+      statuses = @@scouts.map do |scout|
+        scout.build_report(@@reports[scout.class])
+      end
+    end
+  end
+end
