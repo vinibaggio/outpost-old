ac1a7e94f6e0ca3248a78e64fb6e059ffa5ce914
Tomas D'Stefano <tomasdestefi@gmail.com>
2010-09-20 21:57:46 -0300

    Put comments n the consolidate method
    - Renamed some variables

--- lib/outpost/scout/consolidation.rb
+++ lib/outpost/scout/consolidation.rb
@@ -1,21 +1,46 @@
 module Scout
   module Consolidation
+
+    # The status list and their priority is as follows
+    # (from lowest to highest priority):
+    # 
+    #     :up
+    #     :warning
+    #     :unknown
+    #     :down    
+    #    
+    PRIORITY = {
+      :up => 2,
+      :warning => 1,
+      :unknown => 0,
+      :down => -1
+    }
+
+    # Return the consolidate in the priority list priority
+    #
+    # consolidate [:up] # => [:up]
+    # consolidate [:up, :down] # => [:down]
+    # consolidate [nil] # => [:unknown]
+    #
     def consolidate(status_list)
       status_list ||= []
       status_list.compact!
-
+      
       return :unknown if status_list.empty?
-
-      map = {
-        :up => 2,
-        :warning => 1,
-        :unknown => 0,
-        :down => -1,
-      }
-      remap = map.invert
-
-      status = status_list.map { |st| map[st] }.min
-      remap[status]
+      
+      status = collect_min_priority(:status_list => status_list)
+      PRIORITY.invert[status]
     end
+    
+    # Collect the minimum priority in the status list
+    #
+    # collect_min_priority :status_list => [:up] # => 2
+    # collect_min_priority :status_list => [:up, :down] # => -1
+    #
+    def collect_min_priority(options)
+      status_list = options[:status_list]
+      status_list.collect { |status| PRIORITY[status] }.min
+    end
+
   end
 end
