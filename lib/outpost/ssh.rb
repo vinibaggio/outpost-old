2eba422399c135f327eb676ca8cdeb3b6f45a6c2
Tomas D'Stefano <tomasdestefi@gmail.com>
2010-09-28 12:14:46 -0300

    Retry when failing public key/hostbased auth

--- lib/outpost/ssh.rb
+++ lib/outpost/ssh.rb
@@ -1,30 +1,38 @@
 module Server
   class SSH
-    
+        
     def self.connect(server, options={})
       strategies(server, options) do |host, user, connection_strategy|
         Net::SSH.start(host, user, connection_strategy)
       end
     end
 
+    # Load All strategies to connect
+    # Code adapted from Capistrano::SSH
     #
     # Load any SSH configuration files that were specified in the SSH options. This
     # will load from ~/.ssh/config and /etc/ssh_config by default (see Net::SSH
     # for details). Merge the explicitly given ssh_options over the top of the info
-    # from the config file.
+    # from the config.
     #
     # See http://net-ssh.rubyforge.org/ssh/v2/api/index.html for more details.
     #
     def self.strategies(server, options, &block)
       auth_methods = [ %w(publickey hostbased), %w(password keyboard-interactive) ] # Methods to handle authentication (for now I'm only using the first element)
-      ssh_options = Net::SSH.configuration_for(server.host, options.fetch(:config, true)).merge(options)
       user = server.user || options[:user]
       host = server.host
       port = server.port
+      ssh_options = Net::SSH.configuration_for(server.host, options.fetch(:config, true)).merge(options)
       ssh_options.delete(:user) # Don't need user in this Hash
       ssh_options[:port] = port if port
-      yield host, user, ssh_options.merge(:auth_methods => auth_methods.shift,
-        :password => nil, :config => false)
+      password = nil
+      begin
+        yield host, user, ssh_options.merge(:auth_methods => auth_methods.shift, :password => password, :config => false)
+      rescue Net::SSH::AuthenticationFailed
+        raise Net::SSH::AuthenticationFailed if auth_methods.empty?
+        password = options[:password]
+        retry
+      end
     end
     
   end
--- spec/ssh_spec.rb
+++ spec/ssh_spec.rb
@@ -4,8 +4,9 @@ module Server
   describe SSH do
   
     before do
-      @server = server('outpost')
+      @server = server('@outpost')
       @options = { :password => nil, :auth_methods => %w(publickey hostbased), :config => false }
+      Net::SSH.stub!(:configuration_for).and_return({})
     end
   
     it 'connect with the server should only do it once' do
@@ -28,6 +29,28 @@ module Server
       server = server('joker@my_server:4050')
       Net::SSH.should_receive(:start).with(server.host, 'joker', @options.merge({:port => 4050})).and_return(sucess=Object.new)
       Server::SSH.connect(server).should == sucess
+    end
+    
+    it "should pass port in ssh options" do
+      @server.stub!(:user).and_return('default-user')
+      Net::SSH.should_receive(:start).with(@server.host, 'default-user', @options.merge({:port => 4050})).and_return(sucess=Object.new)
+      Server::SSH.connect(@server, :port => 4050).should == sucess
+    end
+    
+    it "should try to connect with the server with the public and after failing should try password" do
+      @server.stub!(:user).and_return('default-user')
+      Net::SSH.stub!(:start).with(@server.host, "default-user", @options).and_raise(Net::SSH::AuthenticationFailed)
+      Net::SSH.should_receive(:start).with(@server.host, "default-user", @options.merge(:password => "secret", :auth_methods => %w(password keyboard-interactive))).and_return(success = Object.new)
+      Server::SSH.connect(@server, :password => "secret").should == success
+    end
+    
+    it "should raise an error when failing all the auth methods" do
+      @server.stub!(:user).and_return('default-user')
+      Net::SSH.should_receive(:start).with(@server.host, "default-user", @options).and_raise(Net::SSH::AuthenticationFailed)
+      Net::SSH.should_receive(:start).with(@server.host, "default-user", @options.merge(:password => "secret", :auth_methods => %w(password keyboard-interactive))).and_raise(Net::SSH::AuthenticationFailed)
+      lambda { 
+        Server::SSH.connect(@server, :password => "secret") 
+      }.should raise_exception(Net::SSH::AuthenticationFailed)
     end
     
   end
